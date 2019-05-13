62f2f774e42a499b8066fbc9298869c3c4c0da74
michael <michael.software.engr@gmail.com>
2019-05-13 01:37:52 -0700

    Add deploy code, fix image source issues.

---
+++ assets/static/coding_challenge/precache-manifest.aad4141d2a871ff0c03d01814c19e37f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c0da215e674c771e3d",
+    "url": "/coding_challenge/static/css/main.b3aaed61.chunk.css"
+  },
+  {
+    "revision": "e8c0da215e674c771e3d",
+    "url": "/coding_challenge/static/js/main.35231970.chunk.js"
+  },
+  {
+    "revision": "96be7c4ed78ce93379d3",
+    "url": "/coding_challenge/static/js/runtime~main.5dce85ad.js"
+  },
+  {
+    "revision": "f49f7e367300c25efeed",
+    "url": "/coding_challenge/static/js/2.2c952038.chunk.js"
+  },
+  {
+    "revision": "41c6ca7ba2edae3562de7b5845944ad3",
+    "url": "/coding_challenge/index.html"
+  }
+];
--- assets/static/coding_challenge/service-worker.js
+++ assets/static/coding_challenge/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae6c24b7c0043961b4cb2626241711b5.js"
+  "/coding_challenge/precache-manifest.aad4141d2a871ff0c03d01814c19e37f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/coding_challenge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
